f9b809d4eae12869c47d7f54d07445030ff67b3b
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-08 09:18:36 -0500

    slightly more markup

--- src/components/sections/SectionCreatePost/index.tsx
+++ src/components/sections/SectionCreatePost/index.tsx
@@ -3,7 +3,13 @@ import image from "/images/illustration-create-post.webp";
 export default function SectionCreatePost() {
   return (
     <section>
-      <h2 data-text-preset="2">Create and schedule content quicker.</h2>
+      <h2 data-text-preset="2">
+        Create and schedule content{" "}
+        <em>
+          <span>quicker.</span>
+        </em>
+      </h2>
+
       <img
         src={image}
         alt="A button that says 'Create Post'. A cursor hovers over it"
