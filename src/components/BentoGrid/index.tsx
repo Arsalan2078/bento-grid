5837ca4f8f80676485ef9d6a06a68186438aeba2
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-08 11:12:49 -0500

    grid layout done

--- src/components/BentoGrid/index.tsx
+++ src/components/BentoGrid/index.tsx
@@ -2,5 +2,5 @@ import styles from "./index.module.css";
 import type { ReactNode } from "react";
 
 export default function BentoGrid({ children }: { children: ReactNode }) {
-  return <div className={styles.grid}>{children}</div>;
+  return <div className={`${styles.container} ${styles.grid}`}>{children}</div>;
 }
